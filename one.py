2a172b135849cf8f0af2b8a8dd6db3e4ffd124c0
Praveen G <25690316+cod3cat@users.noreply.github.com>
2023-09-07 23:09:00 +0530

    Tutorial 1

---
+++ one.py
@@ -0,0 +1,22 @@
+# Test 1 -- Print
+print("Hello World!")
+
+# Test 2 -- Using variables
+print("My name is John")
+print("John is 25 years old")
+print("John likes to eat pizza")
+print("This man's name is John")
+
+print("----------------------")
+name = "Michael"
+age = 25
+print("My name is ", name)
+print(name, "is", age, "years old")
+print(name, "likes to eat pizza")
+print("This man's name is", name)
+
+# Gathering input from the console
+print("----------------------")
+
+answer = input("What is your name? ")
+print("Your name is", answer)
